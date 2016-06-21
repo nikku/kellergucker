0d6f2e540fc93c81b72e5e2a8912864ea3858643
Nico Rehwaldt <git_nikku@nixis.de>
2016-06-21 09:06:24 +0200

    fix(db): switch to fixed precision for rows

--- src/main/resources/db/migration/V20160614__add_stocks_history.sql
+++ src/main/resources/db/migration/V20160614__add_stocks_history.sql
@@ -1,11 +1,11 @@
 CREATE TABLE historic_stocks (
   symbol VARCHAR(255),
   date VARCHAR(255),
-  open real,
-  high real,
-  low real,
-  close real,
-  volume real,
-  adjusted_close real,
+  open numeric(12, 3),
+  high numeric(12, 3),
+  low numeric(12, 3),
+  close numeric(12, 3),
+  volume numeric(12, 3),
+  adjusted_close numeric(12, 3),
   PRIMARY KEY (symbol, date)
 );
